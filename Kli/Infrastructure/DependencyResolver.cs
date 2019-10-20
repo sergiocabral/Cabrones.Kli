8ee70c65253fb7a365aac566add4a5a241a17b7d
Sergio Cabral <sergio.cabral@sapiensia.com>
2019-10-20 15:47:08 -0300

    Adicionado class estática DependencyResolver
    Usando LightInject para resolver interfaces para instâncias.

---
+++ Kli/Infrastructure/DependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using LightInject;
+
+namespace Kli.Infrastructure
+{
+    /// <summary>
+    /// Resolvedor de classes.
+    /// </summary>
+    public static class DependencyResolver
+    {
+        /// <summary>
+        /// Construtor estático. Configuração inicial.
+        /// </summary>
+        static DependencyResolver()
+        {
+            RegisterAssemblies();
+        }
+
+        /// <summary>
+        /// Retorna uma instância do tipo solicitado.
+        /// </summary>
+        /// <typeparam name="TService">Tipo solicitado.</typeparam>
+        /// <returns>Instância encontrada.</returns>
+        public static TService GetInstance<TService>()
+        {
+            return Container.GetInstance<TService>();
+        }
+
+        /// <summary>
+        /// Container de trabalho do LightInject para este projeto.
+        /// </summary>
+        private static readonly ServiceContainer Container = new ServiceContainer();
+
+        /// <summary>
+        /// Registra as interfaces e tipos associados.
+        /// </summary>
+        private static void RegisterAssemblies()
+        {
+        }
+    }
+}
---
+++ Tests/UnitTests/Kli/Infrastructure/LightInjectConfigurationTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using FluentAssertions;
+using Kli.Infrastructure;
+using NSubstitute;
+using Xunit;
+
+namespace Tests.UnitTests.Kli.Infrastructure
+{
+    public class LightInjectConfigurationTest: Test
+    {
+        
+        [Fact]
+        public void deve_existir_o_método_capaz_de_resolver_serviços()
+        {
+            // Arrange, Given
+            
+            var tipo = typeof(DependencyResolver);
+
+            // Act, When
+            
+            var método = tipo.GetMethod("GetInstance", BindingFlags.Static | BindingFlags.Public);
+            var parâmetrosTipoGeneric = método?.GetGenericArguments() ?? Substitute.For<Type[]>();
+
+            // Assert, Then
+            
+            método.Should().NotBeNull();
+            parâmetrosTipoGeneric.Length.Should().Be(1);
+        }
+    }
+}
